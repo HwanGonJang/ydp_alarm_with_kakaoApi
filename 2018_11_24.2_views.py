a189002d493f4a51c4de66b541ad185aed746229
SeoJeongHyun <30520300+rhaxlwo21@users.noreply.github.com>
2018-11-28 22:52:27 +0900

    Update 2018_11_24.2_views.py

--- 2018_11_24.2_views.py
+++ 2018_11_24.2_views.py
@@ -119,13 +119,13 @@ elif(36<=int(pm25) and int(pm25)<=75):
 elif(76<=int(pm25)):
     warning1 = '\n상태 : 매우나쁨\n\n일반인 : 장시간 또는 무리한 실외 활동제한, 기침이나 목의 통증 등이 있는 사람은 실외활동을 피해야 함\n\n민감군 : 가급적 실내 활동만 하고 실외 활동시 의사와 상의'
 
-if(int(pm25)<=30):
+if(int(pm10)<=30):
     warning2 = '\n상태 : 좋음\n\n일반인 : 굳'
-elif(31<=int(pm25) and int(pm25)<=80):
+elif(31<=int(pm10) and int(pm10)<=80):
     warning2 = '\n상태 : 보통\n\n민감군 : 실외활동 시 특별히 행동에 제약은 없으나 몸 상태에 따라 유의하여 활동'
-elif(81<=int(pm25) and int(pm25)<=150):
+elif(81<=int(pm10) and int(pm10)<=150):
    warning2 = '\n상태 : 나쁨\n\n일반인 : 장시간 또는 무리한 실외활동 제한, 특히 눈이 아프거나, 기침, 목의 통증으로 불편한 사람은 실외활동을 피해야 함\n\n민감군 : 장시간 또는 무리한 실외활동 제한, 특히 천식환자는 실외활동 시 흡입기를 더 자주 사용할 필요가 있음'
-elif(151<=int(pm25)):
+elif(151<=int(pm10)):
     warning2 = '\n상태 : 매우나쁨\n\n일반인 : 장시간 또는 무리한 실외 활동제한, 기침이나 목의 통증 등이 있는 사람은 실외활동을 피해야 함\n\n민감군 : 가급적 실내 활동만 하고 실외 활동시 의사와 상의'
 
 resultpm25 = '동작구 초미세먼지: ' + pm25 + warning1 + '\n\nhttps://search.naver.com/search.naver?sm=top_hty&fbm=1&ie=utf8&query=%EB%8F%99%EC%9E%91%EA%B5%AC+%EC%B4%88%EB%AF%B8%EC%84%B8%EB%A8%BC%EC%A7%80 에서 수집된 정보이고, 사이트별로 오차가 있을수 있습니다.'
